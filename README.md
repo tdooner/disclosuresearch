0dacb61ab70e4539d1967bf8d765a7dc7cb2c75e
Tom Dooner <tom.dooner@brigade.com>
2016-05-14 14:22:39 -0700

    Add barebones frontend from react-hot-boilerplate

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # disclosure-search
 
+## Install elasticsearch:
 ```bash
 brew install elasticsearch
 brew services start elasticsearch
 /usr/local/Cellar/elasticsearch/2.3.2/libexec/bin/plugin install mobz/elasticsearch-head
+```
+
+## Install nodejs:
+```bash
+brew update && brew install node-build nodenv
+nodenv install
+npm install
 ```
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app/index'
+  ],
+  resolve: {
+    root: [
+      path.join(__dirname, 'app'),
+    ],
+    extensions: [
+      '',
+      '.js',
+      '.jsx',
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'app')
+    }]
+  }
+};
